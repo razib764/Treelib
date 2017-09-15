f2e042b52e8295e5ec2f68771abc2c95802dfd49
ManaswineeB <bezba004@umn.edu>
2017-09-14 22:47:53 -0500

    Fixed the Graphs
    The graphs of the mean number of arrivals and total time of arrivals v/s rate of arrivals has been fixed. The total time of arrivals v/s lambda gives out an exponential graph and the number of arrivals v/s time gives out a   linear graph

--- Costomer arrival.py
+++ Costomer arrival.py
@@ -62,44 +62,35 @@ import matplotlib.mlab
 import random
 
 arrive_hist = [] #list of means of number of arrivals in a limited time
-time_hist =[] #list of means of total times with a limited number of arrivals
+time_hist =[]#list of means of total times with a limited number of arrivals
+lambda_hist = []
 
 lambdacount = 0.1
 while lambdacount <= 10:
     
     arrive_list = [] #list of arrivals in limited time
     time_list = [] #list of total times with limited arrivals
-
+    lambda_hist += [lambdacount]
+    
     count = 0
     while count < 100:
-        arrive_list += [cust_arrival(1,'t',1000)]
-        time_list += [cust_arrival(1,'n',1000)]
+        arrive_list += [cust_arrival(lambdacount,'t',1000)]
+        time_list += [cust_arrival(lambdacount,'n',1000)]
         count += 1
-        
-    #print (time_list)
-    #print(arrive_list)
     
     sumOfArrivals = 0
     for elem in arrive_list:
         sumOfArrivals += elem
     MeanA = sumOfArrivals/count
-    #print (MeanA)
     arrive_hist += [MeanA]
 
     sumOfTimes = 0
     for elem in time_list:
         sumOfTimes += elem
     MeanT = sumOfTimes/count
-    #print (MeanT)
     time_hist += [MeanT]
-
+    
     lambdacount += 0.1
 
-    
-   
-
-plt.hist(arrive_hist)
-plt.hist(time_hist)
-
-
-        
+plt.plot(lambda_hist, arrive_hist)
+plt.plot(lambda_hist, time_hist)
