66c2215d34e847cace1d17e1d7463635fc341dad
ManaswineeB <bezba004@umn.edu>
2018-02-19 23:31:45 -0600

    Addition of averages in nodes in plot.py
    node_graoh has been updated to take the average of 100 iteration of trees. Graphs are more exponential now.

--- plots.py
+++ plots.py
@@ -1,19 +1,27 @@
 import dendropy
 import matplotlib.pyplot as plt
+import math
 
 
 def node_graph():
-    birth_rate = 0.001
+    birth_rate = 0.01
     nodes = []
     rate = []
-    while birth_rate < 0.01:
-       t = dendropy.model.birthdeath.birth_death_tree(birth_rate,0.0,max_time=100)
-       nodes += [len(t.leaf_nodes())]
+    while birth_rate < 1:
+       i = 0
+       length_avg = 0
+       while i<100:
+           t = dendropy.model.birthdeath.birth_death_tree(birth_rate,0.0,max_time=5)
+           length_avg += len(t.leaf_nodes())
+           i += 1
+       nodes += [math.log(length_avg/100)]
        rate += [birth_rate]
-       birth_rate += 0.001
+       birth_rate += 0.01
     plt.plot(rate, nodes, 'b-')
-    plt.axis([0.0,0.01, 0,10])
+    plt.axis([0.0,1.1, 0,10])
     plt.show()
+
+
 
 def len_graph():
     birth_rate = 0.1
@@ -30,4 +38,4 @@ def len_graph():
     
     
 node_graph()
-len_graph()
+#len_graph()
--- visualization_code.py
+++ visualization_code.py
@@ -3,8 +3,11 @@ import dendropy
 from ete3 import Tree, TreeStyle
 string = dendropy.model.birthdeath.birth_death_tree(1,0.0,max_time=5).as_string(schema = "newick")
 string = string[5:]
+#check if the plotting is wrong or the tree
 t = Tree(string)
 ts = TreeStyle()
 ts.show_leaf_name = True
+ts.show_branch_length = True
+ts.show_branch_support = True
 t.render("mytree.png")
 print('done')
